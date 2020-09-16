db293aac2fd738171b1e14097e255221cf5be6a9
Mauricio Javier Ruiz Torres <xmaurixx_r95@hotmail.es>
2020-09-16 15:37:19 -0300

    Agregar archivos de proyecto.

---
+++ NoticiasAPI/Controllers/NoticiaController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NoticiasAPI.Models;
+using NoticiasAPI.Services;
+
+namespace NoticiasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoticiaController : ControllerBase
+    {
+        private readonly NoticiaService _noticiaService;
+        public NoticiaController(NoticiaService noticiaService)
+        {
+            _noticiaService = noticiaService;
+        }
+        [HttpGet]
+        [Route("obtener")]
+        public IActionResult GetNoticias()
+        {
+            var result = _noticiaService.GetNoticias();
+            return Ok(result);
+        }
+
+        [HttpPost] //api/noticia/agregar
+        [Route("agregar")]
+        public IActionResult AddNoticia([FromBody] Noticia _noticia)
+        {
+            var result = _noticiaService.AgregarNoticia(_noticia);
+            if (result)
+                return Ok(result);
+            else
+                return BadRequest();
+        }
+
+        [HttpPut] //api/noticia/editar
+        [Route("editar")]
+        public IActionResult EditarNoticia([FromBody] Noticia _noticia)
+        {
+            var result = _noticiaService.EditarNoticia(_noticia);
+            if (result)
+                return Ok(result);
+            else
+                return BadRequest();
+        }
+
+        [HttpDelete] //api/noticia/editar
+        [Route("eliminar/{NoticiaID}")]
+        public IActionResult EliminarNoticia(int NoticiaID)
+        {
+            var result = _noticiaService.EliminarNoticia(NoticiaID);
+            if (result)
+                return Ok(result);
+            else
+                return BadRequest();
+        }
+
+
+    }
+}
---
+++ NoticiasAPI/Services/NoticiaService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoticiasAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoticiasAPI.Services
+{
+    public class NoticiaService
+    {
+        private readonly NoticiasDBContext _noticiasDBContext;
+        public NoticiaService(NoticiasDBContext noticiasDBContext)
+        {
+            _noticiasDBContext = noticiasDBContext;
+        }
+
+        public List<Noticia> GetNoticias()
+        {
+            var result = _noticiasDBContext.Noticia.Include(x => x.Autor).ToList();
+            return result;
+        }
+
+        public Boolean AgregarNoticia(Noticia _noticia)
+        {
+            try
+            {
+                _noticiasDBContext.Noticia.Add(_noticia);
+                _noticiasDBContext.SaveChanges();
+                return true;
+            }
+            catch(Exception error)
+            {
+                return false;
+            }
+
+        }
+
+        public Boolean EditarNoticia(Noticia _noticia)
+        {
+            try
+            {
+                var noticiaBD = _noticiasDBContext.Noticia.Where(b => b.NoticiaID == _noticia.NoticiaID).FirstOrDefault();
+                noticiaBD.Titulo = _noticia.Titulo;
+                noticiaBD.Descripcion = _noticia.Descripcion;
+                noticiaBD.Contenido = _noticia.Contenido;
+                noticiaBD.Fecha = _noticia.Fecha;
+                noticiaBD.AutorID= _noticia.AutorID;
+                _noticiasDBContext.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Boolean EliminarNoticia(int NoticiaID)
+        {
+            try
+            {
+                var noticiaBD = _noticiasDBContext.Noticia.Where(b => b.NoticiaID == NoticiaID).FirstOrDefault();
+                _noticiasDBContext.Noticia.Remove(noticiaBD);
+                _noticiasDBContext.SaveChanges();
+                return true;
+            }
+            catch(Exception error)
+            {
+                return false;
+            }
+        }
+    }
+}
